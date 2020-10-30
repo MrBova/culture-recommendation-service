dcf3ecd588b89e585424a21510330d0160c663e1
Дмитрий Иванов <mrneey@gmail.com>
2020-10-30 23:53:19 +0300

    last changes

---
+++ src/components/content/content.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './content.css';
+
+const Content = () => {
+    return (
+        <div className="album text-muted">
+            <div className="container">
+                <div className="row">
+                    <div className="card">
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div className="card">
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div className="card">
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Content;
---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <button>Логин</button>
+            <input />
+        </div>
+    );
+};
+
+export default Header;
