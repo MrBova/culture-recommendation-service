a866425e41f94d10df3244630409a16b7586b6ce
Дмитрий Иванов <mrneey@gmail.com>
2020-10-30 23:59:30 +0300

    last changes

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -3,17 +3,17 @@ import './content.css';
 
 const Content = () => {
     return (
-        <div className="album text-muted">
+        <div className="album">
             <div className="container">
                 <div className="row">
                     <div className="card">
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text">test</p>
                     </div>
                     <div className="card">
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text">test</p>
                     </div>
                     <div className="card">
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text">test</p>
                     </div>
                 </div>
             </div>
